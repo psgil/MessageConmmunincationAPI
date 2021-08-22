4d6c808880732e68087b438c0125a9ede66fa4cb
psgil <psgil>
2021-08-23 03:40:28 +0530

    Add project files.

---
+++ MessageConmmunincation/Data/MessageConmmunincationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MessageConmmunincation.Model;
+
+namespace MessageConmmunincation.Data
+{
+    public class MessageConmmunincationContext : DbContext
+    {
+        public MessageConmmunincationContext (DbContextOptions<MessageConmmunincationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MessageConmmunincation.Model.Message> Message { get; set; }
+    }
+}
---
+++ MessageConmmunincation/Migrations/20210822142500_MessageCommunication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageConmmunincation.Migrations
+{
+    public partial class MessageCommunication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+        }
+    }
+}
